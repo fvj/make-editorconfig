632a5f718ce88fbac261c8bf8d3ff84295c06c3b
Julius von Froreich <fvj@users.noreply.github.com>
2018-02-26 15:56:46 +0100

    fix indent style detection (spaces)

--- src/deduction/indentStyle.js
+++ src/deduction/indentStyle.js
@@ -4,7 +4,9 @@ export default lines => {
 	const tabs = lines
 		.map(line => getIndentation(line, false))
 		.filter(line => line !== '').length
-	const spaces = lines.map(getIndentation).filter(line => line !== '').length
+	const spaces = lines
+		.map(line => getIndentation(line))
+		.filter(line => line !== '').length
 
 	if ((tabs === 0 && spaces === 0) || lines.length === 0 || tabs === spaces)
 		return null
